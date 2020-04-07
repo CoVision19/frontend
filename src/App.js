e910cdd42c646ba0eeb9cccf8d8abf15cff52801
Thomas Hedoux <thomas.hedoux@epitech.eu>
2020-04-07 12:31:18 +0200

    feat: added basic chart example for testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BarExample from './components/bar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CoVision19 (the beginning).
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="App-body">
+        <BarExample />
+      </div>
     </div>
   );
 }
